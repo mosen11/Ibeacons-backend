4a47d2f036504011acdc795149b7bcd79fb3a045
Mosen Shoushteri <mosen.shousteri@kadaster.nl>
2018-02-28 15:24:26 +0100

    first commit.

---
+++ src/main/java/nl/ibeacons/ibeacons/IbeaconsApplication.java
@@ -0,0 +1,12 @@
+package nl.ibeacons.ibeacons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbeaconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbeaconsApplication.class, args);
+	}
+}
